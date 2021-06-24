6f34351734b53f5d1d252d8f3b95dfeab97062e6
Pedro Coelho <79021161+pedrocoelho94@users.noreply.github.com>
2021-06-24 17:15:34 -0300

    update

--- rocketQ/src/router.js
+++ rocketQ/src/router.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
 const RoomController = require('./controllers/RoomController')
-
 const route = express.Router()
 
 route.get('/', (req, res) => res.render('index', {page:'enter-room'}))
