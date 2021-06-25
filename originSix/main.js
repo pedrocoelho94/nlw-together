ceac65cb0607736d739be6694bf3af350afe6e1c
Pedro Coelho <79021161+pedrocoelho94@users.noreply.github.com>
2021-06-25 14:54:50 -0300

    site finalizado

--- originSix/main.js
+++ originSix/main.js
@@ -15,15 +15,14 @@ for (const link of links) {
 }
 
 // mudar o header da página, adicionando sombra quando der scroll
-
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', () => {
+function changeHeaderWhenScroll() {
    window.scrollY >= navHeight
       ? header.classList.add('scroll')
       : header.classList.remove('scroll')
-})
+}
 
 // TESTIMONIALS CAROULSEL SLIDER SWIPER
 // https://swiperjs.com/
@@ -33,14 +32,20 @@ const swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination'
    },
    mousewheel: true,
-   keyboard: true
+   keyboard: true,
+   breakpoints:{
+      767:{
+         slidesPerView: 2,
+         setWrapperSize: true
+      }
+   }
 })
 
 // SCROLLREVEAL
 const scrollReveal = ScrollReveal({
    origin: 'top',
    distance: '30px',
-   duration: 700,
+   duration: 600,
    reset: true
 })
 
@@ -50,7 +55,51 @@ ScrollReveal().reveal(
    #about .image, #about .text,
    #services header, #services .card,
    #testimonials header, #testimonials .testimonials,
-   #contact .text, #contact .links
+   #contact .text, #contact .links,
+   footer .brand, footer .social
 `,
    { interval: 100 }
 )
+
+// BACK TO TOP
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+
+   if (window.scrollY > 560) {
+      backToTopButton.classList.add('show')
+   } else {
+      backToTopButton.classList.remove('show')
+   }
+}
+// Menu Ativo conforme a seção visível na página
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection(){
+   const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+   for(let section of sections){
+      const sectionTop = section.offsetTop
+      const sectionHeight = section.offsetHeight
+      const sectionId = section.getAttribute('id')
+
+      const checkpointStart = checkpoint >= sectionTop
+      const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+      if(checkpointStart && checkpointEnd){
+         document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active')
+      }else{
+         document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active')
+      }
+
+   }
+}
+
+//WHen Scroll
+window.addEventListener('scroll', () => {
+   changeHeaderWhenScroll()
+   backToTop()
+   activateMenuAtCurrentSection()
+})
+
+
